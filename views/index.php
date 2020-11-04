a8329f64961a28383fcf2b7f05c2a96ed0095dff
Andrescc98 <andresjosecc@gmail.com>
2020-11-04 16:40:05 -0400

    agregando estilos

--- views/index.php
+++ views/index.php
@@ -1,3 +1,3 @@
 <?php $this->layout("layout/app", ["title"=>"index"]) ?>
 
-<h1><?= $this->e($name) ?></h1>
+<h1><?= $this->e($name) ?><i class="fa fa-user-astronaut"></i></h1>
--- views/layout/app.php
+++ views/layout/app.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reporter | <?= $this->e($title) ?> </title>
+    <link rel="stylesheet" href="/assets/style/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/style/fontawesome-5/css/all.min.css">
 </head>
 <body>
     <?= $this->section("content") ?>
