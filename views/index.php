a89f960646d37eb8c04f3d3fa0f9fed3929eed77
Andrescc98 <andresjosecc@gmail.com>
2020-11-04 12:02:51 -0400

    generando templates

---
+++ views/index.php
@@ -0,0 +1,3 @@
+<?php $this->layout("layout/app", ["title"=>"index"]) ?>
+
+<h1><?= $this->e($name) ?></h1>
---
+++ views/layout/app.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporter | <?= $this->e($title) ?> </title>
+</head>
+<body>
+    <?= $this->section("content") ?>
+</body>
+</html>
