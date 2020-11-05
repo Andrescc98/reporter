f4b18113d9f87076eeec3bb61ee01103c352a2b6
Andrescc98 <andresjosecc@gmail.com>
2020-11-05 11:53:14 -0400

    agregando mas contents

--- views/layout/app.php
+++ views/layout/app.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporter | <?= $this->e($title) ?> </title>
     <link rel="stylesheet" href="/assets/style/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/style/fontawesome-5/css/all.min.css">
+    <title>Reporter | <?= $this->e($title) ?> </title>
 </head>
 <body>
-    <?= $this->section("content") ?>
+    <?= $this->section("navbar") ?>
+    <main id="app">
+       <?= $this->section("main") ?>
+    </main>
+
+
+    <script src="/assets/style/bootstrap/js/jquery.min.js"></script>
+    <!-- <script src="/assets/style/bootstrap/js/popper.min.js"></script> -->
+    <script src="/assets/style/bootstrap/js/bootstrap.min.js"></script>
+
+    <?= $this->section("scripts") ?>
 </body>
 </html>
