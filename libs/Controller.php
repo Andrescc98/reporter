2006190bc13473bf595cc43ddae8abc10d8037a8
Andrescc98 <andresjosecc@gmail.com>
2020-11-09 10:47:00 -0400

    agregando metodo de respuesta

--- libs/Controller.php
+++ libs/Controller.php
@@ -6,6 +6,7 @@ use League\Plates\Engine;
 
 abstract class Controller
 {
+    private $host = "http://reporter.com";
 
     use Session;
 
@@ -26,5 +27,18 @@ abstract class Controller
     protected function redirect(string $to)
     {
         return header("location:{$to}");
+    }
+
+    protected function response($res, int $code = 200)
+    {
+        http_response_code($code);
+        if($code >= 400){
+            echo json_encode(["error" => true ,"message" => $res]);
+            die();
+        }elseif($code >= 200 and $code < 400){
+            echo json_encode($res);
+            die();
+        }
+
     }
 }
