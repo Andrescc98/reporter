a68a94f78a5717592b3d8e158fc848daa6a64e4f
Andrescc98 <andresjosecc@gmail.com>
2020-11-09 10:47:58 -0400

    metodos de registro

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,10 +1,33 @@
 <?php
-    namespace Controllers;
-    use Libs\Controller;
 
-    class UserController extends Controller{
+namespace Controllers;
 
-        public function login(){
-            echo $this->render("user/login");
-        }
+use Libs\Controller;
+use Libs\Query;
+
+class UserController extends Controller
+{
+
+    public function login()
+    {
+        echo $this->render("user/login");
     }
+
+    public function register()
+    {
+        echo $this->render("user/register");
+    }
+
+    public function post_insert_register()
+    {
+
+        $username = $_POST["username"];
+        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+
+        Query::insert("users", compact("username", "password"));
+
+        $this->response("creado", 201);
+
+        
+    }
+}
