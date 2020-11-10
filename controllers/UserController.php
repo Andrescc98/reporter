d27d5f77e251c8c1a68960a0d43241304fd5dc7b
Andrescc98 <andresjosecc@gmail.com>
2020-11-09 23:08:07 -0400

    metodo de post_login

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -13,6 +13,25 @@ class UserController extends Controller
         echo $this->render("user/login");
     }
 
+    public function post_login()
+    {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $user = Query::select("users", compact("username"));
+        $user = $user[0];
+
+        if(empty($user)){
+            $this->response("no encontrado", 404);
+        }
+        echo json_encode($user);
+        if(!$password or !password_verify($password, $user["password"])){
+            $this->response("contraseña invalida", 422);
+        }
+        $this->start($user);
+        $this->response("", 204);
+    }
+
     public function register()
     {
         echo $this->render("user/register");
