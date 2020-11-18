2c75e20a433e78d6ef5c9919ca047e3e743bf0df
Andrescc98 <andresjosecc@gmail.com>
2020-11-17 23:30:29 -0400

    agregando navbar

--- views/layout/app.php
+++ views/layout/app.php
@@ -3,13 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="/assets/style/bootstrap/css/bootstrap.min.css">
+    <!-- FONTAWESOME -->
     <link rel="stylesheet" href="/assets/style/fontawesome-5/css/all.min.css">
+    <!-- STYLES -->
+    <link rel="stylesheet" href="/assets/style/style.css">
+    <?= $this->section("style") ?>
     <title>Reporter | <?= $this->e($title) ?> </title>
 </head>
 <body>
-    <?= $this->section("navbar") ?>
-    <main id="app">
+    <?php if(!empty($_SESSION)): ?>
+        <?= $this->fetch("partials/navbar") ?>
+    <?php endif?>
+
+    <main id="app" class="mt-5 container-fluid">
        <?= $this->section("main") ?>
     </main>
 
---
+++ views/partials/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand navbar-light bg-light py-3">
+    <a href="/index" class="navbar-brand">Reporter</a>
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-user-friends"></i> Personas
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-search"></i> Buscar
+                </a>
+            </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-user-circle fa-2x"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
