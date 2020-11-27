716676edece8d8f7cfe85643953083f11eab95c1
Andrescc98 <andresjosecc@gmail.com>
2020-11-26 23:46:18 -0400

    solucinado error de require

--- public/reporter/lista.php
+++ public/reporter/lista.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__."/../vendor/autoload.php";
+require __DIR__."/../../vendor/autoload.php";
 use Controllers\ReporterController;
 
 $reporterController = new ReporterController; 
